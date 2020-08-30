4f36e485f357aeef35c49c34a31b763b20de1d37
jackattack <jacquelinenicole@me.com>
2020-08-30 17:18:41 -0600

    stored objects to local storage and added screenshots to README

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,28 @@
 // Establish date and time
 var today = moment();
 $("#currentDay").text(today.format("MMMM Do YYYY, h:mm:ss a"));
+
+// store in local storage
+var tasks = {
+    "hour1": [],
+    "hour2": [],
+    "hour3": [],
+    "hour4": [],
+    "hour5": [],
+    "hour6": [],
+    "hour7": [],
+    "hour8": [],
+};
+console.log(tasks);
+
+var getTasks = function() {
+    var loadedTasks = JSON.parse(localStorage.getItem("tasks"));
+}
+
+
+getTasks();
+
+// save button
+$(".saveBtn").click(function() {
+    replaceTextarea($(this));
+})
